974645ec60b00e7e0148091edf79bc2da5631061
Emmie <emmanueladeremi512@gmail.com>
2025-09-02 11:24:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from '../src/components/Nav'
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='flex flex-col bg-amber-300 w-50 h-50 border-2'>
+      <h1>work in the name of jesus </h1>
+    </div>
+  )
+}
+
+export default Nav
