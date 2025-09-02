2db1a74afa5cf9de70ff1f36cdf73f2159e9b48a
Emmie <emmanueladeremi512@gmail.com>
2025-09-02 11:40:28 +0100

    testingImage

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import Image from "../assets/images/work.png";
 
 const Nav = () => {
   return (
     <div className='flex flex-col bg-amber-300 w-50 h-50 border-2'>
       <h1>work in the name of jesus </h1>
+      <img src={Image} alt='' />
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
